8dbecf9e3da9e5cc7a9e781684130ac3f67417db
WT <ybandyawes@gmail.com>
2018-06-14 13:35:09 +1000

    update mesh.h

--- Hope/mesh.h
+++ Hope/mesh.h
@@ -6,11 +6,23 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+};
+
 class mesh
 {
 private:
 	shaderProgram * sp;
-	
+	unsigned int VAO, VBO, EBO;
+
 public:
 	mesh();
 	~mesh();
