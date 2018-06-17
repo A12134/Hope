f4783041eae5c620c6b9ab636a64595b494f5b85
WT <ybandyawes@gmail.com>
2018-06-17 21:18:26 +1000

    Add shader-application class based communications

--- Hope/shaderProgram.cpp
+++ Hope/shaderProgram.cpp
@@ -54,3 +54,201 @@ void shaderProgram::useThis()
 {
 	glUseProgram(this->programID);
 }
+
+void shaderProgram::setUniform1f(const char * var, float f1)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform1f(loc, f1);
+}
+
+void shaderProgram::setUniform2f(const char * var, float f1, float f2)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform2f(loc, f1, f2);
+}
+
+void shaderProgram::setUniform3f(const char * var, float f1, float f2, float f3)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform3f(loc, f1, f2, f3);
+}
+
+void shaderProgram::setUniform4f(const char * var, float f1, float f2, float f3, float f4)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform4f(loc, f1, f2, f3, f4);
+}
+
+void shaderProgram::setUniform1i(const char * var, int i1)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform1i(loc, i1);
+}
+
+void shaderProgram::setUniform2i(const char * var, int i1, int i2)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform2i(loc, i1, i2);
+}
+
+void shaderProgram::setUniform3i(const char * var, int i1, int i2, int i3)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform3i(loc, i1, i2, i3);
+}
+
+void shaderProgram::setUniform4i(const char * var, int i1, int i2, int i3, int i4)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform4i(loc, i1, i2, i3, i4);
+}
+
+void shaderProgram::setUniform1ui(const char * var, unsigned int ui1)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform1ui(loc, ui1);
+}
+
+void shaderProgram::setUniform2ui(const char * var, unsigned int ui1, unsigned int ui2)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform2ui(loc, ui1, ui2);
+}
+
+void shaderProgram::setUniform3ui(const char * var, unsigned int ui1, unsigned int ui2, unsigned int ui3)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform3ui(loc, ui1, ui2, ui3);
+}
+
+void shaderProgram::setUniform4ui(const char * var, unsigned int ui1, unsigned int ui2, unsigned int ui3, unsigned int ui4)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform4ui(loc, ui1, ui2, ui3, ui4);
+}
+
+void shaderProgram::setUniform1fv(const char * var, glm::vec1 vec1)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform1fv(loc, 1, glm::value_ptr(vec1));
+}
+
+void shaderProgram::setUniform2fv(const char * var, glm::vec2 vec2)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform2fv(loc, 1, glm::value_ptr(vec2));
+}
+
+void shaderProgram::setUniform3fv(const char * var, glm::vec3 vec3)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform3fv(loc, 1, glm::value_ptr(vec3));
+}
+
+void shaderProgram::setUniform4fv(const char * var, glm::vec4 vec4)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform4fv(loc, 1, glm::value_ptr(vec4));
+}
+
+void shaderProgram::setUniform1iv(const char * var, glm::ivec1 ivec1)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform1iv(loc, 1, glm::value_ptr(ivec1));
+}
+
+void shaderProgram::setUniform2iv(const char * var, glm::ivec2 ivec2)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform2iv(loc, 1, glm::value_ptr(ivec2));
+}
+
+void shaderProgram::setUniform3iv(const char * var, glm::ivec3 ivec3)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform3iv(loc, 1, glm::value_ptr(ivec3));
+}
+
+void shaderProgram::setUniform4iv(const char * var, glm::ivec4 ivec4)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform4iv(loc, 1, glm::value_ptr(ivec4));
+}
+
+void shaderProgram::setUniform1uiv(const char * var, glm::uvec1 uvec1)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform1uiv(loc, 1, glm::value_ptr(uvec1));
+}
+
+void shaderProgram::setUniform2uiv(const char * var, glm::uvec2 uvec2)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform2uiv(loc, 1, glm::value_ptr(uvec2));
+}
+
+void shaderProgram::setUniform3uiv(const char * var, glm::uvec3 uvec3)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform3uiv(loc, 1, glm::value_ptr(uvec3));
+}
+
+void shaderProgram::setUniform4uiv(const char * var, glm::uvec4 uvec4)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniform4uiv(loc, 1, glm::value_ptr(uvec4));
+}
+
+void shaderProgram::setUniformMatrix2x2fv(const char * var, glm::mat2 mat2, GLboolean transpose)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniformMatrix2fv(loc, 1, transpose, glm::value_ptr(mat2));
+}
+
+void shaderProgram::setUniformMatrix3x3fv(const char * var, glm::mat3 mat3, GLboolean transpose)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniformMatrix3fv(loc, 1, transpose, glm::value_ptr(mat3));
+}
+
+void shaderProgram::setUniformMatrix4x4fv(const char * var, glm::mat4 mat4, GLboolean transpose)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniformMatrix4fv(loc, 1, transpose, glm::value_ptr(mat4));
+}
+
+void shaderProgram::setUniformMatrix2x3fv(const char * var, glm::mat2x3 mat2x3, GLboolean transpose)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniformMatrix2x3fv(loc, 1, transpose, glm::value_ptr(mat2x3));
+}
+
+void shaderProgram::setUniformMatrix3x2fv(const char * var, glm::mat3x2 mat3x2, GLboolean transpose)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniformMatrix3x2fv(loc, 1, transpose, glm::value_ptr(mat3x2));
+}
+
+void shaderProgram::setUniformMatrix2x4fv(const char * var, glm::mat2x4 mat2x4, GLboolean transpose)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniformMatrix2x4fv(loc, 1, transpose, glm::value_ptr(mat2x4));
+}
+
+void shaderProgram::setUniformMatrix4x2fv(const char * var, glm::mat4x2 mat4x2, GLboolean transpose)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniformMatrix4x2fv(loc, 1, transpose, glm::value_ptr(mat4x2));
+}
+
+void shaderProgram::setUniformMatrix3x4fv(const char * var, glm::mat3x4 mat3x4, GLboolean transpose)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniformMatrix3x4fv(loc, 1, transpose, glm::value_ptr(mat3x4));
+}
+
+void shaderProgram::setUniformMatrix4x3fv(const char * var, glm::mat4x3 mat4x3, GLboolean transpose)
+{
+	GLint loc = glGetUniformLocation(this->programID, var);
+	glUniformMatrix4x3fv(loc, 1, transpose, glm::value_ptr(mat4x3));
+}
--- Hope/shaderProgram.h
+++ Hope/shaderProgram.h
@@ -2,6 +2,9 @@
 #define SHADER_PROGRAM_H
 #include <string>
 #include "LogManager.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class shaderProgram
 {
@@ -29,6 +32,55 @@ public:
 	bool matchName(std::string name) { return name == shaderName; }
 
 	unsigned int getID() { return programID; }
+
+	// Shader communication interface
+public:
+	// floats
+	void setUniform1f(const char* var, float f1);
+	void setUniform2f(const char* var, float f1, float f2);
+	void setUniform3f(const char* var, float f1, float f2, float f3);
+	void setUniform4f(const char* var, float f1, float f2, float f3, float f4);
+
+	// integers
+	void setUniform1i(const char* var, int i1);
+	void setUniform2i(const char* var, int i1, int i2);
+	void setUniform3i(const char* var, int i1, int i2, int i3);
+	void setUniform4i(const char* var, int i1, int i2, int i3, int i4);
+
+	// unsigned integers
+	void setUniform1ui(const char* var, unsigned int ui1);
+	void setUniform2ui(const char* var, unsigned int ui1, unsigned int ui2);
+	void setUniform3ui(const char* var, unsigned int ui1, unsigned int ui2, unsigned int ui3);
+	void setUniform4ui(const char* var, unsigned int ui1, unsigned int ui2, unsigned int ui3, unsigned int ui4);
+
+	// float vector
+	void setUniform1fv(const char* var, glm::vec1 vec1);
+	void setUniform2fv(const char* var, glm::vec2 vec2);
+	void setUniform3fv(const char* var, glm::vec3 vec3);
+	void setUniform4fv(const char* var, glm::vec4 vec4);
+
+	// integer vector
+	void setUniform1iv(const char* var, glm::ivec1 ivec1);
+	void setUniform2iv(const char* var, glm::ivec2 ivec2);
+	void setUniform3iv(const char* var, glm::ivec3 ivec3);
+	void setUniform4iv(const char* var, glm::ivec4 ivec4);
+
+	// unsigned integer vector
+	void setUniform1uiv(const char* var, glm::uvec1 uvec1);
+	void setUniform2uiv(const char* var, glm::uvec2 uvec2);
+	void setUniform3uiv(const char* var, glm::uvec3 uvec3);
+	void setUniform4uiv(const char* var, glm::uvec4 uvec4);
+
+	// float matrix
+	void setUniformMatrix2x2fv(const char* var, glm::mat2 mat2, GLboolean transpose);
+	void setUniformMatrix3x3fv(const char* var, glm::mat3 mat3, GLboolean transpose);
+	void setUniformMatrix4x4fv(const char* var, glm::mat4 mat4, GLboolean transpose);
+	void setUniformMatrix2x3fv(const char* var, glm::mat2x3 mat2x3, GLboolean transpose);
+	void setUniformMatrix3x2fv(const char* var, glm::mat3x2 mat3x2, GLboolean transpose);
+	void setUniformMatrix2x4fv(const char* var, glm::mat2x4 mat2x4, GLboolean transpose);
+	void setUniformMatrix4x2fv(const char* var, glm::mat4x2 mat4x2, GLboolean transpose);
+	void setUniformMatrix3x4fv(const char* var, glm::mat3x4 mat3x4, GLboolean transpose);
+	void setUniformMatrix4x3fv(const char* var, glm::mat4x3 mat4x3, GLboolean transpose);
 };
 #endif
 
